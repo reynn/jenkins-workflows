8ddafea5a059d2d5bfc1449c6bd6776e3189fda1
Nic Patterson <arasureynn@gmail.com>
2017-12-31 06:14:26 -0800

    Add a force option to git commit, fix doc issues.

--- git.groovy
+++ git.groovy
@@ -10,7 +10,7 @@ tools:
     section: git.commit
     name: message
     description: The message to attach to the commit.
-    default: Automatic commit from {{ job_url }}
+    default: "Automatic commit from {{ job_url }}"
   - type: String
     section: git.commit
     name: pattern
@@ -60,6 +60,35 @@ concurUtil      = new Util()
 /*
 description: Execute an Ansible playbook
 parameters:
+  - type: String
+    section: git.commit
+    name: message
+    description: The message to attach to the commit.
+    default: "Automatic commit from {{ job_url }}"
+  - type: String
+    section: git.commit
+    name: pattern
+    description: Pattern for the `git add` command
+    default: "."
+  - type: String
+    section: git.commit
+    name: author
+    description: Author of this commit
+    default: ${env.GIT_AUTHOR} <${env.GIT_EMAIL}>
+  - type: Boolean
+    section: git.commit
+    name: ammend
+    description: Whether to ammend the previous commit.
+    default: false
+  - type: Boolean
+    section: git.commit
+    name: push
+    description: Push the commit to git as well.
+    default: true
+  - type: Map
+    section: git.commit
+    name: credentials
+    description: Credentials to use when pushing to git origin.
 example:
   branches:
     feature:
@@ -76,6 +105,7 @@ public commit(Map yml, Map args) {
   String author   = args?.author      ?: yml.tools?.git?.commit?.author       ?: env.GIT_AUTHOR
   String email    = args?.email       ?: yml.tools?.git?.commit?.email        ?: env.GIT_EMAIL
   Boolean ammend  = args?.ammend      ?: yml.tools?.git?.commit?.ammend       ?: false
+  Boolean force   = args?.force       ?: yml.tools?.git?.commit?.force        ?: false
   Boolean push    = args?.push        ?: yml.tools?.git?.commit?.push         ?: true
   Map credentials = args?.credentials ?: yml.tools?.git?.commit?.credentials
 
@@ -93,7 +123,7 @@ public commit(Map yml, Map args) {
               |&& git config user.email '$email'
               |&& git config push.default simple
               |&& git remote set-url origin "https://${env.GIT_USERNAME}:${env.GIT_PASSWORD}@${env.GIT_HOST}/${env.GIT_ORG}/${env.GIT_REPO}.git"
-              |&& git add $pattern
+              |&& git add ${force ? '-f' : ''} $pattern
               |&& git commit ${ammend ? '--ammend' : ''} -m \"$message\"
               |&& git push origin HEAD:${env.BRANCH_NAME}""".stripMargin()
         }
@@ -104,7 +134,7 @@ public commit(Map yml, Map args) {
               |&& git config user.email '$email'
               |&& git config push.default simple
               |&& git remote set-url origin "git@${env.GIT_HOST}:${env.GIT_ORG}/${env.GIT_REPO}.git"
-              |&& git add $pattern
+              |&& git add ${force ? '-f' : ''} $pattern
               |&& git commit ${ammend ? '--ammend' : ''} -m \"$message\"
               |&& GIT_SSH_COMMAND='ssh -i ${env.GIT_SSH_KEY_FILE}' git push origin HEAD:${env.BRANCH_NAME}""".stripMargin()
         }
